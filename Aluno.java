8ca8ec7252e835b3e076fae0e361c6734dc8ba40
rafa-rodrigues <rodrigues.ok@gmail.com>
2024-01-30 18:29:53 -0300

    Getters and Setters Classe Alunos

--- Aluno.java
+++ Aluno.java
@@ -10,4 +10,20 @@ public class Aluno extends Pessoa{
         this.login = login;
         this.totAssistido = 0;
     }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    }
 }
--- App.java
+++ App.java
@@ -7,6 +7,9 @@ public class App {
         video[1] = new Video("Aula 2 de HTML ");
         video[2] = new Video("Aula 3 de JAVASCRIPT ");
 
+        Aluno aluno = new Aluno("Rafael",37, "M",95.5f);
+
+
         System.out.println(video[0].toString());
         System.out.println(video[1].toString());
         System.out.println(video[2].toString());
